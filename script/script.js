c076828c123087481b85e48704ee6a5056f70af0
Rati Gvazbaia <rgvazbaia@gmail.com>
2024-04-04 10:54:34 +0400

    add pdf

--- script/script.js
+++ script/script.js
@@ -76,6 +76,7 @@ const srLeft = ScrollReveal({
 
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
+srLeft.reveal('.edudiv',{delay: 300})
 
 /* -- ABOUT SKILLS & FORM BOX -- */
 const srRight = ScrollReveal({
