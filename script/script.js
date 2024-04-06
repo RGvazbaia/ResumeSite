1ec2516023b43b9bd200fde3ca48f119cdc27975
Rati Gvazbaia <rgvazbaia@gmail.com>
2024-04-06 19:46:16 +0400

    add js

--- script/script.js
+++ script/script.js
@@ -67,12 +67,12 @@ sr.reveal('.top-header',{})
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
 /* -- ABOUT INFO & CONTACT INFO -- */
-const srLeft = ScrollReveal({
-  origin: 'left',
-  distance: '80px',
-  duration: 2000,
-  reset: true
-})
+// const srLeft = ScrollReveal({
+//   origin: 'left',
+//   distance: '80px',
+//   duration: 2000,
+//   reset: true
+// })
 
 srLeft.reveal('.about-info',{delay: 100})
 srLeft.reveal('.contact-info',{delay: 100})
